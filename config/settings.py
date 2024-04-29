4cc8284e46e422045920e8b7bc22d615a6a9872a
Mary Melnikova <marymelnikova1988@gmail.com>
2024-04-29 12:08:25 +0300

    Курсовая 8. Docker

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,9 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# django
+# dot_env = os.path.join(BASE_DIR, '.env.local')
+# doker
 dot_env = os.path.join(BASE_DIR, '.env')
 load_dotenv(dotenv_path=dot_env)
 
